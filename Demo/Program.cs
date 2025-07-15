06e8e004577dd1803081cb878cc7f79608242c5d
m7md9923 <mhmdnsrbrkat0@gmail.com>
2025-07-15 13:16:20 +0300

    Vid 3

---
+++ Demo/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace Demo;
+using Demo.Overloading;
+class Program
+{
+    #region Method Overloading
+
+    // public static int SumTwoNumbers(int a, int b)
+    // {
+    //     return a + b;
+    // }
+    // public static double SumTwoNumbers(double a, double b)  // method overloading
+    // {
+    //     return a + b;
+    // }
+    // public static int SumThreeNumbers(int a, int b, int c)
+    // {
+    //     return a + b + c;
+    // }
+    //
+    // public static double SumThreeNumbers(double a, double b, double c)
+    // {
+    //     return a + b + c;
+    // }
+    //
+    // public static int SumNumbers(int a, int b)
+    // {
+    //     return a + b;
+    // }
+    //
+    // public static double SumNumbers(double a, double b)
+    // {
+    //     return a + b;
+    // }
+    //
+    // public static int SumNumbers(double a, int b)
+    // {
+    //     return (int)a + b;
+    // }
+    //
+    // public static int SumNumbers(int a, int b, int c)
+    // {
+    //     return a + b + c;
+    // }
+    // public static double SumNumbers(double a, double b, double c)
+    // {
+    //     return a + b + c;
+    // }
+    #endregion
+    
+    static void Main(string[] args)
+    {
+        #region Demo
+        
+        #region Method Overloading - Polymorphism
+        //int x = 10, y = 20 , z = 30;
+        // Console.WriteLine(SumTwoNumbers(x,y));
+        // Console.WriteLine(SumThreeNumbers(x, y, z));
+        
+        
+        //double a = 10.5, b = 20.5, c = 30.5;
+        // Console.WriteLine(SumTwoNumbers(a,b));
+        // Console.WriteLine(SumThreeNumbers(a, b, c));
+
+        // Console.WriteLine(SumNumbers(x, y));
+        // Console.WriteLine(SumNumbers(a , x));
+        // Console.WriteLine(SumNumbers(x,  y, z));
+        // Console.WriteLine(SumNumbers(a ,b , c));
+        #endregion
+        
+        #endregion
+    }
+}
