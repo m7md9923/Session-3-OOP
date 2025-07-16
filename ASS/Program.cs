c9d2f5c9e5e62fee73d6bb88399809afc5ea1af9
m7md9923 <mhmdnsrbrkat0@gmail.com>
2025-07-16 17:26:01 +0300

    Part01 [Q1]

---
+++ ASS/Program.cs
@@ -0,0 +1,27 @@
+﻿using ASS.Classes;
+namespace ASS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Assigment 3
+            
+            #region Part01
+
+            #region q1
+
+            //  Calculator calculator = new Calculator();
+            // Console.WriteLine( calculator.Add(1, 2)); 
+            // Console.WriteLine( calculator.Add(1, 2, 3));
+            // Console.WriteLine( calculator.Add(1.5, 2.5));
+
+            #endregion
+            
+            
+            #endregion
+            #endregion
+        }
+        
+    }
+}
