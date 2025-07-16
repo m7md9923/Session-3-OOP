4e580cb934f04e1ab40b4ef2803976d1cf04f417
m7md9923 <mhmdnsrbrkat0@gmail.com>
2025-07-16 18:34:24 +0300

    Part 02

---
+++ ASS/Classes/Duration.cs
@@ -0,0 +1,158 @@
+﻿namespace ASS.Classes
+{
+    internal class Duration
+    {
+        #region Attributes
+        public int Hours { get; set; }
+
+        public int Minutes { get; set; }
+
+        public int Seconds { get; set; }
+        #endregion
+        #region Methods 
+        public override string ToString()
+        {
+            return $"Hours: {Hours} ,Minutes: {Minutes} , Seconds: {Seconds} ";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Duration d)
+            {
+                return Hours == d.Hours && Minutes == d.Minutes && Seconds == d.Seconds;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Hours, Minutes, Seconds);
+        }
+
+        public Duration(int hours, int minutes, int seconds)
+        {
+            Hours = hours;
+            Minutes = minutes;
+            Seconds = seconds;
+        }
+        public Duration(int seconds)
+        {
+            Hours = seconds / 3600;
+            seconds %= 3600;
+            Minutes = seconds / 60;
+            Seconds = seconds % 60;
+        }
+        public Duration()
+        {
+        }
+
+        public static Duration operator +(Duration d1, Duration d2)
+        {
+            return new Duration()
+            {
+                Hours = (d1?.Hours ?? 0) + (d2?.Hours ?? 0),
+                Minutes = (d1?.Minutes ?? 0) + (d2?.Minutes ?? 0),
+                Seconds = (d1?.Seconds ?? 0) + (d2?.Seconds ?? 0)
+            };
+        }
+
+        public static Duration operator +(Duration d1, int S)
+        {
+            Duration d2 = new Duration(S);
+            return new Duration()
+            {
+                Hours = (d1?.Hours ?? 0) + (d2?.Hours ?? 0),
+                Minutes = (d1?.Minutes ?? 0) + (d2?.Minutes ?? 0),
+                Seconds = (d1?.Seconds ?? 0) + (d2?.Seconds ?? 0)
+            };
+        }
+        public static Duration operator +(int S, Duration d1)
+        {
+            Duration d2 = new Duration(S);
+            return new Duration()
+            {
+                Hours = (d1?.Hours ?? 0) + (d2?.Hours ?? 0),
+                Minutes = (d1?.Minutes ?? 0) + (d2?.Minutes ?? 0),
+                Seconds = (d1?.Seconds ?? 0) + (d2?.Seconds ?? 0)
+            };
+        }
+        public static Duration operator -(Duration d1, Duration d2)
+        {
+            return new Duration()
+            {
+                Hours = (d1?.Hours ?? 0) - (d2?.Hours ?? 0),
+                Minutes = (d1?.Minutes ?? 0) - (d2?.Minutes ?? 0),
+                Seconds = (d1?.Seconds ?? 0) - (d2?.Seconds ?? 0)
+            };
+        }
+
+        public static Duration operator ++(Duration d1)
+        {
+            return new Duration()
+            {
+                Hours = (d1?.Hours ?? 0),
+                Minutes = (d1?.Minutes ?? 0) + 1,
+                Seconds = (d1?.Seconds ?? 0)
+            };
+        }
+        public static Duration operator --(Duration d1)
+        {
+            return new Duration()
+            {
+                Hours = (d1?.Hours ?? 0),
+                Minutes = (d1?.Minutes ?? 0) - 1,
+                Seconds = (d1?.Seconds ?? 0)
+            };
+        }
+
+        public static bool operator >(Duration d1, Duration d2)
+        {
+            if (d1 == null || d2 == null) 
+                return false;
+            if (d1.Hours > d2.Hours) 
+                return true;
+            else if (d1.Hours == d2.Hours)
+            {
+                if (d1.Minutes > d2.Minutes)
+                    return true;
+                else if (d1.Minutes == d2.Minutes)
+                    return d1.Seconds > d2.Seconds;
+            }
+            return false;
+        }
+        public static bool operator <(Duration d1, Duration d2)
+        {
+            if (d1 == null || d2 == null)
+                return false;
+
+            if (d1.Hours < d2.Hours)
+                return true;
+            else if (d1.Hours == d2.Hours)
+            {
+                if (d1.Minutes < d2.Minutes)
+                    return true;
+                else if (d1.Minutes == d2.Minutes)
+                    return d1.Seconds < d2.Seconds;
+            }
+            return false;
+        }
+        public static bool operator <=(Duration d1, Duration d2)
+        {
+            return !(d1 > d2);
+        }
+        public static bool operator >=(Duration d1, Duration d2)
+        {
+            return !(d1 < d2);
+        }
+        public static implicit operator bool(Duration d1)
+        {
+            return d1 != null && (d1.Hours >= 0 || d1.Minutes >= 0 || d1.Seconds >= 0);
+        }
+
+        public static explicit operator DateTime(Duration d1)
+        {
+            return new DateTime(1, 1, 1, d1.Hours, d1.Minutes, d1.Seconds);
+        }
+        #endregion
+
+    }
+}
--- ASS/Program.cs
+++ ASS/Program.cs
@@ -64,6 +64,51 @@ namespace ASS
             #endregion
             
             #endregion
+            
+            #region part 02
+
+            //Duration D1 = new Duration(1, 10, 15);
+            //Console.WriteLine(D1.ToString());
+
+            //Duration D2 = new Duration(3600);
+            //Console.WriteLine(D2.ToString());
+
+
+            //Duration D3 = new Duration(666);
+            ////Console.WriteLine(D3.ToString());
+
+            ////Duration D4 = new Duration(7800);
+            ////Console.WriteLine(D4.ToString());
+
+            //D3 = D1 + D2;
+            //Console.WriteLine(D3.ToString());
+
+            //D3 = D1 + 7800;
+            //Console.WriteLine(D3.ToString());
+            //D3 = 666 + D3;
+            //Console.WriteLine(D3.ToString());
+            //D3 = ++D1;
+            //Console.WriteLine(D3.ToString());
+            //D3 = --D2;
+            //Console.WriteLine(D3.ToString());
+            //D1 = D1 - D2;
+            //Console.WriteLine(D1.ToString());
+            //if (D1 > D2)
+            //    Console.WriteLine("D1>D2");
+            //if(D1 <= D2)
+            //    Console.WriteLine("D1<=D2");
+            //else
+            //    Console.WriteLine("D1>D2");
+            //D1 = null;
+            //if(D1)
+            //    Console.WriteLine("D1 is not null");
+            //else
+            //    Console.WriteLine("D1 is null");
+            //DateTime Obj = (DateTime)D1;
+
+
+            #endregion
+            
             #endregion
         }
         
